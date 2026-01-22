79aa122188b4d978e42a84ccd23fe30f9487cf52
RyuOuO <ryuouo@example.com>
2026-01-22 12:23:11 +0800

    Fix NameError: scrolledtext is not defined

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import messagebox, filedialog
+from tkinter import messagebox, filedialog, scrolledtext
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 import subprocess
